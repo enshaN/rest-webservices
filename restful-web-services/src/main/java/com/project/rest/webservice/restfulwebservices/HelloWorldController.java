d36dc8f08053cd16fca9df5142e2709dde263752
ensha <ensha@cybercesdatauni>
2020-04-10 20:44:49 -0700

    rest controller functionality

--- restful-web-services/src/main/java/com/project/rest/webservice/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/project/rest/webservice/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,12 @@
 package com.project.rest.webservice.restfulwebservices;
 
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class HelloWorldController {
 	
-	
+	@RequestMapping(method=RequestMethod.GET,path="/helloworld")
 public  String HelloWorld() {
 	
 	return "Hello World";
