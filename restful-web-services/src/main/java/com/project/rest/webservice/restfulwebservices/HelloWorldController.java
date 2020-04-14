3ecbc71906265ec621a5d94a7f400aba38b0d801
ensha <ensha@cybercesdatauni>
2020-04-14 00:39:31 -0700

    enhancing Helloworld to return a bean

--- restful-web-services/src/main/java/com/project/rest/webservice/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/project/rest/webservice/restfulwebservices/HelloWorldController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping(method=RequestMethod.GET,path="/helloworld")
+@RequestMapping(method=RequestMethod.GET,path="/helloworld")
 public  String HelloWorld() {
 	
 	return "Hello World";
+}
+
+@RequestMapping(method=RequestMethod.GET,path="/hello-world-bean")
+public HelloWorldBean GetHelloWorld() {
+	return new HelloWorldBean ("helloworldbean");
 }
 
 
